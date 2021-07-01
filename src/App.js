ce6e8a7bb4aa3a47b61525c7ddc367a7673f2b95
pooulad <poooooladi@gmail.com>
2021-07-01 20:05:00 +0430

    App.js completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
+import { UserContext } from "./components/UserContext";
+import AddUser from "./components/AddUser";
+import ListUser from "./components/ListUser";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const dispatchUserEvent = (action, data) => {
+    switch (action) {
+      case "AddUser":
+        setUsers([...users, data.newUser]);
+        return;
+      case "DeleteUser":
+        setUsers(users.filter(user => user.id !== data.userId));
+        return;
+      default:
+        return;
+    }
+  }
+
   return (
     <div className="App">
-      react
+      <UserContext.Provider value={{ users }}>
+        <AddUser />
+        <ListUser />
+      </UserContext.Provider>
     </div>
   )
 }
